dbbacbeb40e92ccfb68af6c0499065d506775d11
Yuki Niwano <alc1944zi.dl-em@sky.plala.or.jp>
2024-08-23 10:40:04 +0900

    scoreのmodel定義

--- database/migrations/2024_08_21_181756_create_scores_table.php
+++ database/migrations/2024_08_21_181756_create_scores_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('scores', function (Blueprint $table) {
             $table->id(); #点数ID
-            $table->foreignId('middle_id')->constrained(); #中間IDを参照
             $table->foreignId('subject_id')->constrained(); #教科IDを参照
             $table->integer('score'); #点数
         });
